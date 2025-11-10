4d156278df12e740e4f8e2cc5ff1ffe3d1b53fbf
mintblancmanche <lavenlutz1015@gmail.com>
2025-11-10 13:42:36 +0900

    file moved

--- lualumiere/Encounter/Action_Newmission.lua
+++
@@ -1,8 +0,0 @@
---[[
-ScenEdit_RunScript ("lualumiere/Encounter/Action_Newmission.lua")
-]]
-
-local guid = ScenEdit_UnitX().guid
-local mission = "Mining"
-ScenEdit_AssignUnitToMission (guid, "None") 
-ScenEdit_AssignUnitToMission (guid, mission) 
--- lualumiere/Encounter/Action_SetDepth.lua
+++
@@ -1,8 +0,0 @@
---[[
-ScenEdit_RunScript ("lualumiere/Encounter/Action_SetDepth.lua")
-]]
-
-ScenEdit_RunScript ("lualumiere/UnitSub_SetDepth.lua")
-
-SetDepth({name='B-112', guid='cc554ab2-c046-488f-b993-b63e86841e84'})
-SetDepth({name='B-546', guid='f8b527b4-359e-4d21-a90b-ea0e161ac59c'})
